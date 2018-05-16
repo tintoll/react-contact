47a167ef7b015e36ffaf7e863541e1fa0ce3bc1c
blue <blue@tigrison.com>
2018-05-16 11:12:14 +0900

    모달 틀에해당되는 Modal 컴포넌트 추가 (모달보여주기,숨기기,애니매이션 담당)

---
+++ src/components/Modal.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import onClickOutside  from 'react-onclickoutside';
+import PropTypes from 'prop-types';
+import {media} from '../lib/styled-utils';
+
+
+// 모달 위치 및 사이즈 설정
+const Wrapper = styled.div`
+  /* 레이아웃 */ 
+  position : fixed;
+  /* 화면 가운데 정렬 */
+  top : 50%;
+  left : 50%;
+  transform : translate(-50%, -50%);
+
+  /* 레이어 */
+  z-index : 10;
+
+  /* 너비 기본은 400px */
+  width : ${ props => prop.width };
+
+  /* 모바일일경우에는 양옆에 여백 1rem에 꽉 채우기 */
+  ${media.mobile`
+    width : clac(100% - 2rem);
+  `}
+`;
+Wrapper.propTypes = {
+  width : PropTypes.string
+}
+
+// 모달틀 
+const ModalBox = styled.div`
+  background : white;
+  border : 1px solid rgba(0,0,0,0.3);
+`
+
+class Modal extends Component {
+  static propTypes ={
+    visible : PropTypes.bool,
+    onHide : PropTypes.func,
+    width: PropTypes.string,
+  }
+  static defaultProps = {
+    width : '400px'
+  }
+
+  // 컴포넌트 외부를 클리하면 실행되는 메서드
+  handleClickOutside = (e) => {
+    const { visible, onHide } = this.props;
+    if(!visible) return null; // 이미 visible이 false라면 아무것도 안함
+    onHide();
+  }
+  // ESC 키가 클릭되면 onHide를 실행한다
+  handleKeyUp = (e) => {
+    const { onHide } = this.props;
+    if(e.keyCode === 27) {
+      onHide();
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    // visible 값이 변할때
+    if(prevProps.visible !== this.props.visible) {
+
+      if(this.props.visible) {
+        // body 에 keyUp 이벤트 등록해서 ESC키를 감지한다.
+        document.body.addEventListener('keyup', this.handleKeyUp);
+      } else {
+        document.body.removeEventListener('keyup', this.handleKeyUp);
+      }
+    }
+  }
+  
+  render() {
+    const { visible, children, width } = this.props;
+    return (
+      <div>
+        <Wrapper width={width}>
+          {
+            /* visible이 참일때만 ModalBox 보여줌 */
+            visible && (<ModalBox>{children}</ModalBox>)
+          }
+        </Wrapper>
+      </div>
+    )
+  }
+}
+// onClickoutside 라이브러리 적용
+export default onClickOutside(Modal);
